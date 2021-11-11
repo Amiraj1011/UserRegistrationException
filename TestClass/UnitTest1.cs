d9bc970e6801f91dc133f1e161745a02dbd66b18
“AMIR <amiraj1011@gmail.com>
2021-11-11 11:57:37 -0800

    amir UC1 for handling exception of user first name

---
+++ TestClass/UnitTest1.cs
@@ -0,0 +1,27 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UserRegistrationException;
+
+namespace ExceptionTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void ValidFirstNameOfUser()
+        {
+            //AAA method
+            //Arrange
+            string message = " ";
+            string expected = "invalid";
+            //creating object of moodanalyzer class and passing message
+            UserException exception = new UserException(message);
+
+            //Act
+            string actual = exception.ValidFirstName();
+
+            //Assert
+            //comparing actual and expected value
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
